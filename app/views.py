f8b362db14d08cc6baa0f98c19b82c649aa71ba0
sirgama <sirengogamaliel@gmail.com>
2022-04-30 23:43:34 +0300

    add article page fuction and file

--- app/views.py
+++ app/views.py
@@ -10,3 +10,11 @@ def index():
     '''
     message = 'Testing the news site'
     return render_template('index.html', message= message)
+
+@app.route('/article/<int:id>')
+def article(id):
+    
+    '''
+    returns the article page
+    '''
+    return render_template('article.html' )
