5d667caf03b8a065c9a759559c3e6559319de495
sirgama <sirengogamaliel@gmail.com>
2022-05-02 10:24:23 +0300

    add source html app route

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from .request import get_news, get_sites
+from .request import get_news, get_sites, get_source
 
 #Views for the app
 
@@ -11,13 +11,14 @@ def index():
     '''
     news = get_news()
     sites = get_sites()
+    sources = get_source()
     message = 'Testing the news site'
-    return render_template('index.html', message= message, articles= news ,channels = sites)
+    return render_template('index.html', message= message, articles= news ,channels = sites, medias = sources)
 
-@app.route('/article/<int:id>')
-def article(id):
+@app.route('/source/<int:source>')
+def source(source):
     
     '''
-    returns the article page
+    returns the source page
     '''
-    return render_template('article.html' )
+    return render_template('source.html' )
